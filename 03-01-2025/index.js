56e7cf8ff8459b514294fb39c8fdd72e457fcccc
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-01-03 21:49:11 +0530

    let const var difference analysed

--- 03-01-2025/index.js
+++ 03-01-2025/index.js
@@ -1,1 +1,17 @@
 console.log('Welcome to node js');
+// {
+//     const a = 10;
+// }
+// console.log(a);
+
+// {
+//     let x = 20;
+// }
+// console.log(x);
+
+{
+    var y = 30;
+}
+console.log(y);
+
+const name = 'John';
