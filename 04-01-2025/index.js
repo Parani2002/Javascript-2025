1706012a55dea20285dfd7bdf7c6b83dcabf109d
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-01-12 20:01:10 +0530

    foreach tried

--- 04-01-2025/index.js
+++ 04-01-2025/index.js
@@ -1,1 +1,38 @@
 console.log("euhdiuegidu");
+const name = "paranitharan";
+
+var Createcounter = function(n){
+    return function(){
+        return n++
+    }
+}
+
+const counter = Createcounter(20);
+console.log(typeof(counter));
+
+console.log(counter());
+console.log(counter());
+console.log(counter());
+
+var createCounter = function(n){
+    return function increment(){
+        return ++n;
+    }
+    return function  decrement(){
+        return --n;
+    }
+    return function reset(){
+        return init;
+    }
+}
+const x = createCounter(10);
+console.log(x.increment)
+
+const names = ["parani","suntharam","mano"];
+names.forEach(element => {
+    console.log(element);
+    
+});
+
+
+
