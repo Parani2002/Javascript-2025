402dee74f3e5d97ddd51f59c9bef153854af14ba
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-01-22 22:23:14 +0530

    add tailwind css styling

--- jquery/index.js
+++ jquery/index.js
@@ -2,21 +2,17 @@ $(document).ready(function(){
     $('#btnAdd').click(function(){
         var input = $('#input1').val();
         if(input){
-            $("ul").append('<li>' + input + ' <button class="btnDelete">Delete</button></li></br>');
+            $("ul").append('<li>' + input + '<button class="btnDelete">Delete</button></li></br>');
             $('#input1').val('');
             $('.count').text($('li').length);
         }else{
             $('.alert').show('slow');
             $('#input1').focus();
-            setTimeout(function(){
-                $('.alert').hide('slow');
-            }, 200);
-
+            $('.alert').hide('slow');
         }
     })
     $('ul').on('click', '.btnDelete', function () {
-        $(this).closest('li').remove(); // Remove the corresponding list item
-        $('.count').text($('li').length);
+        $(this).closest('li').remove();  // Remove the corresponding list item
+        $('.count').text($('li').length); 
     });
-    
 })
