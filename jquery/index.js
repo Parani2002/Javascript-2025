29d1c0adeaf76431a739c7355138b86e5cc72f39
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-01-22 19:28:22 +0530

    display the length of list

--- jquery/index.js
+++ jquery/index.js
@@ -2,11 +2,21 @@ $(document).ready(function(){
     $('#btnAdd').click(function(){
         var input = $('#input1').val();
         if(input){
-            $("ul").append('<li>' + input + ' <button class="btnDelete">Delete</button></li>');
+            $("ul").append('<li>' + input + ' <button class="btnDelete">Delete</button></li></br>');
             $('#input1').val('');
+            $('.count').text($('li').length);
+        }else{
+            $('.alert').show('slow');
+            $('#input1').focus();
+            setTimeout(function(){
+                $('.alert').hide('slow');
+            }, 200);
+
         }
     })
     $('ul').on('click', '.btnDelete', function () {
         $(this).closest('li').remove(); // Remove the corresponding list item
+        $('.count').text($('li').length);
     });
+    
 })
