d9ad38e248afc9e326098358ddf0c8b5b997e264
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-01-26 11:47:04 +0530

    notes file created

---
+++ jquery/notes.js
@@ -0,0 +1,26 @@
+// we can use the $ or jQuery sign to call the jQuery function
+$('document').ready(function(){
+
+})
+//id selector
+$('#input1').val();
+$('#input1').text();
+
+//class selector
+$('.btnAdd').click(function(){
+    alert('Button clicked');
+})
+$('.input1').val();
+
+//element selector
+$('p').text();
+
+//Attribute selector
+$('[placeholder]').hide();
+$('[name = "input1"]').hide();
+
+//add content to HTML
+$('p').append('Hello World');
+
+//remove content from HTML
+$('p').remove('Hello World');
