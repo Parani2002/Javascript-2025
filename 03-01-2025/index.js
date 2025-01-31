17276c1acab1d9fcd587269bc046ebe218c2407f
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-01-31 21:08:34 +0530

    lexical scope added

--- 03-01-2025/index.js
+++ 03-01-2025/index.js
@@ -28,4 +28,39 @@ const name = 'John';
 // const a = 10;
 // const a = 20;
 
-confirm("Parani")
+// confirm("Parani")
+var outside = "I am outsideeee";
+
+//lexical scoping
+function outerFunction(){
+    var outer = "I am outside!";
+    function innerFunction(){
+        var inner = "I am inside!";
+        console.log(outer);
+        console.log(inner);
+    }
+    innerFunction();
+    console.log(outer);
+    //console.log(inner);
+}
+
+outerFunction();
+
+//Remember that lexical scope means definition space — not invocation space.
+
+const printName = function(){
+    const fullname = "Paranitharan Suntharalingam within the function"; //variable definition
+    console.log(fullname); //variable usage
+    
+}
+
+const fullname = "Paranitharan"; //it is global scope but it is called inside function.
+const printFullname = function(){
+    console.log(fullname); 
+}
+
+console.log(fullname); // it is lexical scope because it is called outside the function.global scope.
+
+printName();
+printFullname();
+
