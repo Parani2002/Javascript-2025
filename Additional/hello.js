f3de31f2903ca4874679910bef6cdf1df9bb6920
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-02-01 21:49:29 +0530

    variable hoisting

--- Additional/hello.js
+++ Additional/hello.js
@@ -18,22 +18,34 @@ const {display, add} = require('./index')
 
 // console.log(add(12,22));
 
-const add = (a,b) => {
-    return a+b;
-}
-const substract = (a,b) => {
-    return a-b;
-}
-const multiply = (a,b) => {
-    return a*b;
-}   
-const divide = (a,b) => {
-    return a/b;
-}
-const power = (a,b) => {
-    return Math.pow(a,b);
-}
+// const add = (a,b) => {
+//     return a+b;
+// }
+// const substract = (a,b) => {
+//     return a-b;
+// }
+// const multiply = (a,b) => {
+//     return a*b;
+// }   
+// const divide = (a,b) => {
+//     return a/b;
+// }
+// const power = (a,b) => {
+//     return Math.pow(a,b);
+// }
 
-module.exports = {add, substract, multiply, divide, power};
+// module.exports = {add, substract, multiply, divide, power};
+
+
+// for(let i = 0; i < 10; i++){
+//    if(i === 3) break;
+//     console.log(i);
+// };
+console.log(a);
+console.log(b);
+
+var a = 10;
+let b ;
+
 
 
