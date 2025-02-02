b7a79ace73740a1f306c525dd09fb6648c4bef8b
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-02-02 08:21:01 +0530

    response header

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -3,5 +3,22 @@ console.log("Welcome to NodeJS");
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-    
-})
+    const url = req.url;
+    if(url === '/'){    
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><h1>Welcome to NodeJS</h1></body>');
+        res.write('</html>');
+        res.end();
+    }else if(url === '/about'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>About</title></head>');
+        res.write('<body><h1>About Page</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+      
+});
+server.listen(8000);
