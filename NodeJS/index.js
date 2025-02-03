2d07ed039d9daca23e7ab3891b42ce12a160bf4f
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-02-03 21:50:13 +0530

    chunks in stream

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -8,8 +8,6 @@ const server = http.createServer((req,res) => {
     const method = req.method;
     if(url === '/'){    
         res.setHeader('Content-Type','text/html');
-        console.log("dwwefewfew");
-        
         res.write('<html>');
         res.write('<head><title>Enter details</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><input type="submit" value="Send"/></form></body>');
@@ -18,10 +16,22 @@ const server = http.createServer((req,res) => {
         
     }
     if(url === '/message' && method === 'POST'){
+        const body = [];
+       req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+       });
+       //when request sends all chunk data.
+       req.on('end',() => {
+        const parsedBody = Buffer.concat(body).toString();
+        console.log(parsedBody);
+        const message = parsedBody.split('=')[1];
+        fs.writeFileSync('message.txt',message);
         fs.writeFileSync('message.txt','DUMMY');
         res.setHeader('Location','/');
         res.statusCode = 302;
-        return res.end();
+       })
+       return res.end();
      
     }
         res.setHeader('Content-Type','text/html');
