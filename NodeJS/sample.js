ee7452e45c606721056dc656617f06b930dd7037
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-02-04 20:45:44 +0530

    try catch

--- NodeJS/sample.js
+++ NodeJS/sample.js
@@ -1,6 +1,17 @@
 const http = require('http');
 
 http.createServer((req,res) => {
-    res.write("Paranitharan");
-    res.end();
-}).listen(8000);
+    try{
+        res.writeHead(200,{'Content-Type':'text/plain'});
+        res.write("Paranitharan");
+        res.write("v     ");
+        res.end('okay');
+    }catch(error){
+        res.writeHead(500,{'Content-Type':'text/plain'});
+        res.write(error);
+    }
+    
+}).listen(8000, () => {
+    console.log("Server is running on port 8000");
+});
+
