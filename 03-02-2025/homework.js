623f560efa9d3f4d5cb4982c79c15699bb4d7712
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-02-05 20:53:22 +0530

    date JS object tried

--- 03-02-2025/homework.js
+++ 03-02-2025/homework.js
@@ -16,9 +16,9 @@ const getSalaries = function(...args){
 //get salaries
 salaries = getSalaries(90000,80000,30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000, 110000, 120000);
 // console.log(salaries);
-
+var date = new Date();
 //function to display salary info
-const displaySalaryInfo = () => {
+const displaySalaryInfo = (name="Paranitharan") => {
     let totalSalary = 0;
     let taxAmount = 0;
     if(salaries.length > 0){
@@ -28,7 +28,8 @@ const displaySalaryInfo = () => {
         totalSalary >= 500000 ? taxAmount = totalSalary * 0.2 : taxAmount = totalSalary * 0.15;
     const netSalary = totalSalary - taxAmount;
     console.log("*---------Salary Info-----------*");
-    
+    console.log(`Employee Name : ${name}`);
+    console.log(`Printed Date : ${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`);
     console.log(`1. Total Salary : ${totalSalary}`);
     console.log(`2. Tax Amount : ${taxAmount}`);
     console.log(`3. Net Salary : ${netSalary}`); 
