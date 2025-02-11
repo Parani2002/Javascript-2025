ee4dc8fabc231cedfea3f6d9c9c7a63eee26a551
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-02-11 20:54:47 +0530

    add function added

--- DOM/TodoApp/index.js
+++ DOM/TodoApp/index.js
@@ -1,7 +1,20 @@
-console.log(document.getElementById('task').textContent);
+// console.log(document.getElementById('task').textContent);
 var input = document.getElementById('task');
 
 
-input.addEventListener('input', function(){
-    console.log(input.value)
-})
+// input.addEventListener('input', function(){
+//     console.log(input.value)
+// })
+
+var addBtn = document.getElementById('addTask');
+addBtn.addEventListener('click', function(){
+    var task = document.getElementById('task').value;
+    if(task){
+        const li = document.createElement('li');
+        const ul = document.getElementById('taskList');
+        li.innerHTML = `<p>${input.value.trim()}</p>`;
+        ul.appendChild(li);
+        input.value = '';
+
+    } 
+});
