2864d8c125e6d17b72729906dc107c972a1cd220
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-02-13 10:03:35 +0530

    diff between innertext and textcontent

--- DOM/practice.js
+++ DOM/practice.js
@@ -8,4 +8,18 @@ div.append("Hello Paranitharan","ewrfrge")
 const p = document.createElement('p');
 div.appendChild(p);
 
-p.append("This is a paragraph");
+const heading = document.createElement('h1');
+div.appendChild(heading);
+
+// p.append("This is a paragraph");
+p.innerText = "This is a paragraph";
+heading.textContent = "This is a heading";
+
+console.log(div.innerText); //it prints the text content of the element consider spaces and new lines
+
+console.log(div.textContent); //it prints the text content of the element. it displays the text content of the element without considering spaces and new lines.
+
+// console.log(div.innerHTML);
+
+
+
