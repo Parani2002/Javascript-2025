cb690a8301ee2b95d6aa821c9cc610707d0f0402
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-02-13 09:31:28 +0530

    DOM methods created

---
+++ DOM/practice.js
@@ -0,0 +1,11 @@
+const body = document.body;
+
+const div = document.createElement('div');
+body.append(div);
+div.style.backgroundColor = "red";
+div.append("Hello Paranitharan","ewrfrge")
+
+const p = document.createElement('p');
+div.appendChild(p);
+
+p.append("This is a paragraph");
