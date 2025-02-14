6491a6eb77e4114067e07908b1753d48c7c8296d
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-02-14 06:28:44 +0530

    callback-settimeout added

---
+++ 14-02-2025/index.js
@@ -0,0 +1,18 @@
+function add(a,b){
+    return a+b;
+}
+
+function sub(a,b){
+    return a-b;
+}
+
+function calc(a,b,func){
+    let value = func(a,b);
+    console.log(value);
+    
+}
+
+calc(10,20,add);
+calc(200,100,sub);
+
+setTimeout(() => console.log("This is settimeout function"),3000);
