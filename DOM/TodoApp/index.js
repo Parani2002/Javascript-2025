18a210599ebf513ccfefc8cd227f1aa29ced4e5e
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-02-17 21:44:26 +0530

    updates on todo

--- DOM/TodoApp/index.js
+++ DOM/TodoApp/index.js
@@ -5,16 +5,16 @@ var input = document.getElementById('task');
 var addBtn = document.getElementById('addTask');
 //creating delete button element and set attributes.
 const delBtn = document.createElement('button');
-delBtn.setAttribute(id)
+
 
 addBtn.addEventListener('click', function(){
     var task = document.getElementById('task').value;
     if(task){
         const li = document.createElement('li');
         const ul = document.getElementById('taskList');
-        li.innerHTML = `<p>${input.value.trim()}</p>`;
+        li.innerHTML = `<p>${input.value.trim()}</p><button>Delete</button>`;
         ul.appendChild(li);
-        li.appendChild(delBtn);
+       
         input.value = '';
-    } 
+    }
 });
