7faa48db3597d1fb1bcd4d71b886a7ca88e56f79
Suntharalingam Paranitharan <paranitharanparani13@gmail.com>
2025-02-17 21:56:22 +0530

    add tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./JQuery/**/*.{html,js}"],
+  content: ["./JQuery/**/*.{html,js}","./DOM/**/*.{html,js}"],
   theme: {
     extend: {},
   },
